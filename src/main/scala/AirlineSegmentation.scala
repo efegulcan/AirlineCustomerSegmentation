f64231e7221943605406f02bb88b427ad2dc09be
Efe Tümay Gülcan <gulcan.efetumay@student.atilim.edu.tr>
2024-07-15 00:23:33 +0300

    Added csv logic and output files.

--- src/main/scala/AirlineSegmentation.scala
+++ src/main/scala/AirlineSegmentation.scala
@@ -1,5 +1,6 @@
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.expressions.Window
 
 object AirlineSegmentation {
 
@@ -19,11 +20,72 @@ object AirlineSegmentation {
       col("Gender"),
       col("Age"),
       col("visits"),
-      lit(avgVisits).alias("avgVisits"),
       when(col("visits") > avgVisits, lit("Frequent Visitor"))
         .otherwise(lit("Infrequent Visitor"))
         .alias("Segment")
-    ).orderBy(desc("visits"))
+    ).orderBy(desc("visits")).drop("avgVisits")
+  }
+
+  def mostFrequentTravelMonths(df: DataFrame): DataFrame = {
+    val dfWithDate = df.withColumn(
+      "Departure Date",
+      coalesce(
+        to_date(col("Departure Date"), "M/d/yyyy"),
+        to_date(col("Departure Date"), "MM-dd-yyyy")
+      )
+    )
+
+    dfWithDate.filter(col("Departure Date").isNull)
+      .select("Departure Date")
+      .distinct()
+      .show(false)
+
+    val dfWithMonth = dfWithDate
+      .withColumn("Month", date_format(col("Departure Date"), "MMMM"))
+      .withColumn("Year", year(col("Departure Date")))
+
+    val monthlyCounts = dfWithMonth.groupBy("Country Name", "Month")
+      .count()
+      .withColumnRenamed("count", "Frequency")
+
+    val rankedMonths = monthlyCounts
+      .withColumn("Rank", rank().over(Window.partitionBy("Country Name").orderBy(desc("Frequency"))))
+
+    rankedMonths
+      .filter(col("Rank") === 1)
+      .select("Country Name", "Month", "Frequency")
+      .orderBy("Country Name")
+  }
+
+  def segmentPassengersByAge(df: DataFrame): DataFrame = {
+    df.withColumn("Age Group",
+        when(col("Age") < 18, "Under 18")
+          .when(col("Age").between(18, 25), "18-25")
+          .when(col("Age").between(26, 35), "26-35")
+          .when(col("Age").between(36, 45), "36-45")
+          .when(col("Age").between(46, 60), "46-60")
+          .otherwise("60+"))
+      .groupBy("Age Group")
+      .count()
+      .orderBy(desc("count"))
+  }
+
+  def segmentPassengersByGender(df: DataFrame): DataFrame = {
+    df.groupBy("Gender")
+      .count()
+      .orderBy(desc("count"))
+  }
+
+  def segmentPassengersByNationality(df: DataFrame): DataFrame = {
+    df.groupBy("Nationality")
+      .count()
+      .orderBy(desc("count"))
+  }
+
+  def segmentFlightsByStatus(df: DataFrame): DataFrame = {
+    df.groupBy("Flight Status")
+      .count()
+      .orderBy(desc("count"))
   }
 
   def showDataFrame(df: DataFrame, numRows: Int = 20): Unit = {
--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -12,11 +12,40 @@ object App {
 
     val cleanedData = reader.getCleanedDataFrame
 
+    println("Cleaned Data:")
     AirlineSegmentation.showDataFrame(cleanedData)
 
     val segmentedData = AirlineSegmentation.segmentPassengers(cleanedData)
+    segmentedData.coalesce(1).write.mode("overwrite").option("header", "true").csv("data/output/SegmentedData.csv")
 
+    println("Segmented Data:")
     AirlineSegmentation.showDataFrame(segmentedData)
+
+    val frequentTravelMonthsData = AirlineSegmentation.mostFrequentTravelMonths(cleanedData)
+    frequentTravelMonthsData.coalesce(1).write.mode("overwrite").option("header", "true").csv("data/output/FrequentTravelMonthsData.csv")
+
+    println("Most Frequent Travel Months for Each Country:")
+    AirlineSegmentation.showDataFrame(frequentTravelMonthsData)
+
+    val ageSegmentedData = AirlineSegmentation.segmentPassengersByAge(cleanedData)
+    ageSegmentedData.coalesce(1).write.mode("overwrite").option("header", "true").csv("data/output/AgeSegmentedData.csv")
+    println("Age Segmented Data:")
+    AirlineSegmentation.showDataFrame(ageSegmentedData)
+
+    val genderSegmentedData = AirlineSegmentation.segmentPassengersByGender(cleanedData)
+    genderSegmentedData.coalesce(1).write.mode("overwrite").option("header", "true").csv("data/output/GenderSegmentedData.csv")
+    println("Gender Segmented Data:")
+    AirlineSegmentation.showDataFrame(genderSegmentedData)
+
+    val nationalitySegmentedData = AirlineSegmentation.segmentPassengersByNationality(cleanedData)
+    nationalitySegmentedData.coalesce(1).write.mode("overwrite").option("header", "true").csv("data/output/NationalitySegmentedData.csv")
+    println("Nationality Segmented Data:")
+    AirlineSegmentation.showDataFrame(nationalitySegmentedData)
+
+    val flightStatusSegmentedData = AirlineSegmentation.segmentFlightsByStatus(cleanedData)
+    flightStatusSegmentedData.coalesce(1).write.mode("overwrite").option("header", "true").csv("data/output/FlightStatusSegmentedData.csv")
+    println("Flight Status Segmented Data:")
+    AirlineSegmentation.showDataFrame(flightStatusSegmentedData)
 
     spark.stop()
   }
